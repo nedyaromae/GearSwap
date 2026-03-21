7081ed6bcb61944d945e15f0ed90faf97fe86fb9
nedyaromae <iamapatheticweeb@gmail.com>
2026-03-21 09:55:00 -0400

    priority fix

--- data/Eamorayden/Eamorayden_Pld_Gear.lua
+++ data/Eamorayden/Eamorayden_Pld_Gear.lua
@@ -334,7 +334,7 @@ function init_gear_sets()
         back={name="Null Shawl",priority=7},waist={name="Sailfi Belt +1",priority=7},legs={name="Sakpata's Cuisses",priority=114},feet={name="Sakpata's Leggings",priority=68}}
 
       sets.engaged.DT = {ammo={name="Coiste Bodhar",priority=3},
-        head={name="Sakpata's Helm",priority=91},neck={name="Warder's Charm +1",priority=2},ear1={name="Alabaster Earring",priority=5},ear2={name="Crep. Earring",priority=6},
+        head={name="Sakpata's Helm",priority=91},neck={name="Warder's Charm +1",priority=2},ear1={name="Alabaster Earring",priority=100},ear2={name="Crep. Earring",priority=6},
         body={name="Sakpata's Plate",priority=136},hands={name="Sakpata's Gauntlets",priority=91},ring1={name="Shadow Ring",priority=8},ring2={name="Moonlight Ring",priority=110},
         back={name="Null Shawl",priority=7},waist={name="Plat. Mog. Belt",priority=300},legs={name="Sakpata's Cuisses",priority=114},feet={name="Sakpata's Leggings",priority=68}}
 	--------------------------------------
